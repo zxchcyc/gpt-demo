bb67360619aec4342a28bc24a2859b03fcfa0c98
xiaochang.zheng <xiaochang.zheng@zatech.com>
2023-04-17 16:25:06 +0800

    init

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Typography, Grid, Divider } from '@mui/material';
+import React, {useState} from 'react';
+import { Button, Typography, Grid, Divider } from '@mui/material';
 import ReactMarkdown from 'react-markdown';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const renderMarkdown = ({ value }) => {
   // 定制渲染器
@@ -33,6 +34,8 @@ const renderMarkdown = ({ value }) => {
 };
 
 const ChatMessage = ({ message, idx }) => {
+  const [copied, setCopied] = useState(false);
+
   return (
     <>
       <Grid item xs={12} style={{ backgroundColor: '#fffaf2', padding: 10 }}>
@@ -42,6 +45,10 @@ const ChatMessage = ({ message, idx }) => {
       <Grid item xs={12} style={{ backgroundColor: '#fff5e6', padding: 10 }}>
         <Typography variant='h6'>A{idx + 1}:</Typography>
         {renderMarkdown({ value: message.output })}
+        <CopyToClipboard text={message.output}
+          onCopy={() => setCopied(true)}>
+          <Button size="small" variant="outlined">{copied ? '已复制' : '复制'}</Button>
+        </CopyToClipboard>
       </Grid>
       <Divider style={{ margin: '20px 0' }} />
     </>
