447862e6b68dba996247a7cf5cfaf8e84ac3e49a
xiaochang.zheng <xiaochang.zheng@zatech.com>
2023-04-17 15:39:18 +0800

    init

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -11,21 +11,21 @@ const renderMarkdown = ({ value }) => {
       children={value}
       components={{
         code({node, inline, className, children, ...props}) {
-          const match = /language-(\w+)/.exec(className || '')
+          const match = /language-(\w+)/.exec(className || '');
           return !inline && match ? (
             <SyntaxHighlighter
               {...props}
               children={String(children).replace(/\n$/, '')}
               style={solarizedlight}
               language={match[1]}
-              PreTag="div"
-              showLineNumbers={true} // 是否展示左侧行数
+              PreTag='div' // 双引号改为单引号
+              showLineNumbers={true} // 是否展示左边行数
             />
           ) : (
             <code {...props} className={className}>
               {children}
             </code>
-          )
+          );
         }
       }}
     />
@@ -35,12 +35,12 @@ const renderMarkdown = ({ value }) => {
 const ChatMessage = ({ message, idx }) => {
   return (
     <>
-      <Grid item xs={12}>
-        <Typography variant="h6">Q{idx + 1}:</Typography>
+      <Grid item xs={12} style={{ backgroundColor: '#fffaf2', padding: 10 }}>
+        <Typography variant='h6' >Q{idx + 1}:</Typography>
         {renderMarkdown({ value: message.input })}
       </Grid>
-      <Grid item xs={12}>
-        <Typography variant="h6">A{idx + 1}:</Typography>
+      <Grid item xs={12} style={{ backgroundColor: '#fff5e6', padding: 10 }}>
+        <Typography variant='h6'>A{idx + 1}:</Typography>
         {renderMarkdown({ value: message.output })}
       </Grid>
       <Divider style={{ margin: '20px 0' }} />
